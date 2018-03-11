dec1ccffabdc75c953bb6a87dbf573d9fcb5b647
Zack Munk <zamunk@gmail.com>
2018-03-11 12:15:52 -0600

    added subtract function

--- firstTest.py
+++ firstTest.py
@@ -1,8 +1,10 @@
+def subVars(x,y):
+    return x-y
 def addVars(x,y):
     return x+y
 
 def main():
-    print('hello world',addVars(19,18))
+    print('hello world',addVars(19,18),subVars(18,9))
 
 
 main()
